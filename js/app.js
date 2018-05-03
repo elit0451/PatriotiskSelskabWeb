5013f765b2c52c2936b056c1e79dda30c7340937
Elitsa Marinovska <elitsamarinovska@Elitsas-MacBook-Pro.local>
2018-05-03 18:47:18 +0200

    Updated controllers(year, block and subBlock), added json files for data recourse

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('mainApp', ['ngRoute']);
 
-app.config(function($routeProvider, $locationProvider) {
+app.config(function($routeProvider) {
     $routeProvider
 
     .when('/', {
@@ -8,12 +8,32 @@ app.config(function($routeProvider, $locationProvider) {
         controller : 'mainController'
     })
 
-    .when('/block', {
+    .when('/year/:year', {
         templateUrl : 'year.html',
-        controller : 'mainController'
-    });
+        controller : 'yearController'
+    })
 
-    $locationProvider.html5Mode(true);
+    .when('/block/:blockID', {
+        templateUrl : 'block.html',
+        controller : 'blockController'
+    })
+
+    .when('/subBlock/:subBlockID', {
+        templateUrl : 'subBlock.html',
+        controller : 'subBlockController'
+    })
+
+    .when('/trialGroup/:trialGroupID', {
+        templateUrl : 'trialGroup.html',
+        controller : 'trialGroupController'
+    })
+    
+    .otherwise({
+        controller : function(){
+            window.location.replace('/');
+        }, 
+        template : "<div></div>"
+    });
 });
 
 app.filter('unique', function() {
@@ -32,43 +52,74 @@ app.filter('unique', function() {
     };
  });
 
-app.controller('mainController', function($rootScope, $scope){
-        $scope.setSelectedFieldBlockYear = function(newFieldBlock){
-            $rootScope.selectedFieldBlockYear = newFieldBlock;
-        };
+app.controller('mainController', function($rootScope, $scope, $http){
 
-    $scope.FieldBlocks = [
-          {
-            "FieldBlockID": "1",
-            "BlockChar": "A",
-            "FieldBlockYear": 2018,
-            "FieldBlockLength": 270,
-            "FieldBlockWidth": 320,
-            "Comment": "This is a field comment"
-          },
-          {
-            "FieldBlockID": "2",
-            "BlockChar": "B",
-            "FieldBlockYear": 2018,
-            "FieldBlockLength": 640,
-            "FieldBlockWidth": 110,
-            "Comment": "This is a field comment"
-          },
-          {
-            "FieldBlockID": "3",
-            "BlockChar": "A",
-            "FieldBlockYear": 2017,
-            "FieldBlockLength": 640,
-            "FieldBlockWidth": 110,
-            "Comment": "This is a field comment"
-          },
-          {
-            "FieldBlockID": "4",
-            "BlockChar": "A",
-            "FieldBlockYear": 2016,
-            "FieldBlockLength": 239,
-            "FieldBlockWidth": 320,
-            "Comment": "This is a field comment"
-          }
-        ];
+    $http.get("data/FieldBlocks.json")
+  .then(function(response) {
+    $rootScope.FieldBlocks = response.data;
+  });
+    
+});
+
+app.controller('yearController', function($scope, $routeParams, $rootScope, $http){
+    $rootScope.selectedFieldBlockYear = $routeParams.year;
+    $http.get("data/SubBlocks.json")
+  .then(function(response) {
+    $rootScope.SubBlocks = response.data;
+  });
+});
+
+app.controller('blockController', function($scope, $routeParams, $http, $rootScope){
+    angular.forEach($rootScope.FieldBlocks, function(value, key){
+        if(value.FieldBlockID == $routeParams.blockID){
+            $scope.FieldBlock = value;
+        }
+    });
+    
+    $rootScope.selectedFieldBlockChar = $scope.FieldBlock.BlockChar;
+    $rootScope.selectedFieldBlockID = $scope.FieldBlock.FieldBlockID;
+});
+
+app.controller('subBlockController', function($scope, $rootScope, $routeParams, $http){
+    $scope.TrialGroups = [];
+    
+    $http.get("data/TrialGroups.json")
+    .then(function(response) {
+        $rootScope.TrialGroups = response.data;
+        $http.get("data/Treatment.json")
+        .then(function(response) {
+            $rootScope.Treatments = response.data;
+
+            angular.forEach($rootScope.TrialGroups, function(value, key){
+                if($routeParams.subBlockID == value.SubBlockID){
+                    $scope.TrialGroups.push(value);
+                }
+            });
+
+            angular.forEach($scope.TrialGroups, function(value, key){
+                angular.forEach($rootScope.Treatments, function(treatmentValue, treatmentKey){
+                    if(value.TrialGroupID == treatmentValue.TrialGroupID){
+                        value.Treatment = treatmentValue;
+                }
+            });
+        });
+    });
+});
+
+    angular.forEach($rootScope.SubBlocks, function(value, key){
+        if(value.SubBlockID == $routeParams.subBlockID){
+            $scope.SubBlock = value;
+        }
+    });
+
+    $rootScope.selectedSubBlockChar = $scope.SubBlock.SubBlockChar;
+    $rootScope.selectedSubBlockID = $scope.SubBlock.SubBlockID; 
+});
+
+app.controller('trialGroupController', function($scope, $rootScope, $routeParams, $http){
+    $http.get("http://10.140.101.13:63780/api/TrialGroups/" + $routeParams.subBlockID)
+    .then(function(response) {
+        $scope.TrialGroup = response.data;
+        $rootScope.selectedTrialGroupID = $scope.TrialGroup.TrialGroupID;
+    });    
 });
